7af0f256e19db5cba2da85b21118babaa9304b73
Hashir346 <ahmedhashir346@gmail.com>
2025-02-16 06:23:39 -0800

    2/16/25

---
+++ 14-02-2025/index.js
@@ -0,0 +1,51 @@
+// console.log("hello world")
+
+// var student;
+
+// student = 12;
+
+// var studentRollNo1 = 242;
+// console.log(studentRollNo, '01')
+// var studentRollNo2 = 241;
+
+// console.log(studentRollNo, '03')
+
+// console.log("98800")
+
+// var names = 'MuhammadFareed';
+// console.log(names)
+
+// var compare = 23 < 45;
+
+
+// var newNumber = "dasdada";
+// console.log(Number(newNumber), "01");
+// console.log(+newNumber, "02");
+
+// var array = []
+// var object = {}
+
+// number
+// bigint
+// string
+// Boolean
+// undefined
+// Array
+// Object
+// null
+
+// alert("hello I am Fareed")
+
+// var userName = prompt("what is your name?");
+
+// console.log(userName)
+
+// var number1 = prompt("enter a number");
+// var number2 = prompt("enter a number2");
+
+// console.log(number1 - number2);
+
+
+var isBoss = confirm("Are you the boss?");
+
+alert(isBoss)
---
+++ 16-02-2025/index.js
@@ -0,0 +1,93 @@
+// var isPresent = 23;
+// var number = 23;
+
+// console.log(number !== isPresent);
+// console.log(number === isPresent);
+// console.log(number == isPresent);
+// console.log(number >= isPresent);
+// console.log(number =< isPresent);
+// console.log(number = isPresent);
+
+//console.log("a" > "A")
+
+// console.log(false == 0) : true
+// console.log(false === 0) : false
+// console.log(false == "0") : true
+
+
+// console.log(false == "") : true
+// console.log(0 == "") : true
+// console.log("" == 0) : true
+
+// var userInput = +prompt("Enter your age?")
+
+// if (userInput >= 18) {
+//     alert("your eligilble for university")
+// } else {
+//     alert("your not eligilble for university")
+// }
+
+
+// var userInput = +prompt("enter your marks")
+
+// var totalMarks = 1100;
+// var requiredPer = (userInput / totalMarks) * 100;
+// console.log(requiredPer, "requiredPer");
+
+// if (requiredPer >= 80) {
+//     alert("your eligilble for university")
+// } else {
+//     alert("your not eligilble for university")
+// }
+
+
+
+// var userInput = +prompt("Enter your age?")
+// && :AND dono ka true hona zaroori he
+// || :OR  dono me se ek ka true hona zaroori he
+
+// if (userInput <= 18 || userInput < 30) {
+//     alert("your eligilble for university")
+// } else {
+//     alert("your not eligilble for university")
+// }
+
+
+
+// var userInput = +prompt("enter your marks")
+
+// var totalMarks = 1100;
+// var requiredPer = (userInput / totalMarks) * 100;
+// console.log(requiredPer, "requiredPer");
+
+
+// if (requiredPer >= 50 && requiredPer < 60) {
+//     alert("your grade is C")
+// } else if (requiredPer >= 60 && requiredPer < 70) {
+//     alert("your grade is B")
+// } else if (requiredPer >= 70 && requiredPer < 80) {
+//     alert("your grade is A")
+// } else if (requiredPer >= 80) {
+//     alert("your grade is A+")
+// } else {
+//     alert("your grade is F")
+// }
+
+
+
+// var amount = +prompt("enter amount")
+
+// if (amount > 1500) {
+//     var discount = amount * 0.1;
+//     amount = amount - discount;
+//     // amount -= discount;
+// }
+// console.log(amount, "amount")
+
+
+
+// var firstName = "addad";
+// var lastName = "";
+// var nickName = "SuperCoder";
+
+// console.log(firstName || lastName || nickName || "Anonymous");
