ac6f13e9dec8a21fb28f6306b7b70333e3046949
Hashir346 <ahmedhashir346@gmail.com>
2025-02-24 06:29:14 -0800

    23/2/25

---
+++ 21-02-2025/index.js
@@ -0,0 +1,35 @@
+// var userInput = "";
+// var alternativeName = "User"
+// // console.log(userInput.length)
+// // alert(`Name: ${userInput}`)
+// // alert("Name: " + userInput.length > 0 || alternativeName)
+
+
+// var firstName = prompt("enter your first Name")
+// var lastName = prompt("enter your last Name")
+
+
+// if (firstName && lastName) {
+//     alert("form submit")
+// } else {
+//     alert("please fill form")
+// }
+
+// 0 = red
+// 1 = yellow
+// 2 = green
+
+// var input = 324242;
+// Math.random()
+
+// Math.round(Math.random() * 100000)
+
+// console.log(Math.ceil(23.000001), "ceil")
+// console.log(Math.ceil(12.999999), "ceil")
+// console.log(Math.floor(2.999999), "floor")
+console.log(Math.floor(23.000001), "floor")
+
+
+// Math.abs(-10)
+
+// Math.PI
