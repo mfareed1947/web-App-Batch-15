a49781a8011b3be7231182698e75cf876b4df999
Hashir346 <ahmedhashir346@gmail.com>
2025-02-24 06:23:52 -0800

    24/2/25

---
+++ 23-02-2025/index.js
@@ -0,0 +1,102 @@
+// console.log("hello world")
+
+// Global scope
+// var numberOne = 23;
+
+
+// block scope
+// if (true) {
+//     let numberTwo = 56;
+// }
+// console.log(numberTwo, "adada");
+
+// var vs let vs const
+
+// VAR
+// redeclare
+// update
+
+// var a = 23;
+// // var a = 67;
+// a = 12;
+
+// console.log(a)
+
+
+//LET
+// redclare nhi hoga
+// update hojye ga
+
+
+// let a = 23;
+// a = 56;
+// console.log(a)
+
+
+// CONST
+
+// const a = 21;
+// a = 34;
+
+// console.log(a)
+
+// hoisting
+
+// console.log(a)
+
+// let a;
+
+// const a = 23;
+
+
+
+// console.log(x)
+
+// var x = 12;
+
+
+// var n = 4;
+// function add(num) {
+//     return num + num
+// }
+// var addOne = add(n)
+// var add2 = add(8)
+
+
+
+
+// var a = 5;
+
+//prefix
+// --a;
+// ++a;
+
+// console.log(a)
+// postfix
+// a++;
+// a--;
+
+// console.log(a);
+
+// var a = 6;
+// var b = a++;
+// console.log(b);
+// console.log(a);
+// console.log(b);
+
+// var a = 23
+// var b = ++a + a++ + ++a;
+// console.log(b)
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+// }nic == databaseNic
+// intialize , condition,increment
+
+// for (var i = 0; i <= 10; i++) {
+//     if (i == 5) {
+//         continue
+//     }
+//     console.log(i)
+// }
