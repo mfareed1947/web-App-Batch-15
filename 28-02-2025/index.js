9cc263699f30ae42414f440d8580713a91000abc
Hashir346 <ahmedhashir346@gmail.com>
2025-03-01 02:16:52 -0800

    1/3/25

---
+++ 28-02-2025/index.js
@@ -0,0 +1,40 @@
+
+// for (let i = 0; i < 10; i++) {
+
+//     if (i == 5) {
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+
+
+// let i = 0
+// while (i < 10) {//5
+//     if (i == 5) { // 5 == 5 = true
+//         console.log("adadadd", i)
+//     }
+//     else {
+//         i++; // 5
+//     }
+// }
+
+
+
+// let i = 0
+// while (i < 10) {
+
+//     if (i == 5) {
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+
+// let i = 0
+// do {
+//     console.log(i)
+//     i++;
+// } while (i < 0);
+
+
