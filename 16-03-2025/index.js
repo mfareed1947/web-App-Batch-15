2b9d4f61727e80385030aa081ac7c0e7297763d7
Hashir346 <ahmedhashir346@gmail.com>
2025-03-17 10:15:19 -0700

    array push method

---
+++ 16-03-2025/index.js
@@ -0,0 +1,17 @@
+let students = ["ali", "ahmed", "raza", "waleed"]
+// students.push("noman", "muneeb")
+// students.pop("ali")
+let indexing = students.length - 1
+
+let num = 12;
+let num1 = 30;
+
+num1 = num
+
+// console.log(num1)
+
+let arr = [1, 2, 3, 4, 5]
+let arr2 = arr
+arr2.push(6)
+
+console.log(arr)
