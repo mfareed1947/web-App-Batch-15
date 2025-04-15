db377ac32065b2d8172899398ea436079bf1f322
Hashir346 <ahmedhashir346@gmail.com>
2025-04-15 11:45:33 -0700

    factorial , findindex

---
+++ 13-04-2025/index.js
@@ -0,0 +1,84 @@
+// let factorialRec = (n) => {
+//   if (n === 0 || n == 1) {
+//     return 1;
+//   } else {
+//     return n * factorialRec(n - 1);
+//   }
+// };
+
+// console.log(factorialRec(5));
+
+//
+// 5 * ( 4 * ( 3 * ( 2 * ( 1 = factorialRec(2-1)))))
+
+// let number = 6;
+// function factorial(n:(elem,index,arr)=>{}) {
+//   let fac = 1;
+//   for (let i = 1; i <= n; i++) {
+//     fac = fac * i;
+//   }
+//   return fac;
+// }
+
+// console.log(factorial(number));
+
+// 1. fac = 1
+// 2. fac = 2
+// 3. fac = 6
+// 4. fac = 24
+// 5. fac = 120
+// 6. fac = 720
+
+// let arr = [21, 2354, 54, 234, 544, 323];
+// let students = ["ali", "ahmed", "raza", "muneeb"];
+// console.log(
+//   students.findIndex((num, ind, obj) => {
+//     console.log(num, "value");
+//     console.log(ind, "index");
+//     console.log(obj, "obj");
+//   }),
+//   "hello"
+// );
+
+// console.log(
+//   arr.findIndex((num) => {
+//     console.log(num);
+//   }),
+//   "hello"
+// );
+// findIndex(value: number, index: number, obj: number[])
+
+// students.findIndex();
+
+// function xyz(predicate: (elem, index, arr) => {}) {
+//   for (let i = 0; i < students.length; i++) {}
+//   predicate(students[i], i, students);
+// }
+
+// xyz((elem) => {
+//   console.log(elem);
+// });
+
+// let qwerty = () => {
+//   return "hI";
+// };
+// qwerty();
+
+// function add (){
+
+// }
+
+// const array1 = [5, 12, 8, 44, 130];
+
+// console.log(
+//   array1.findIndex((element, index) => {
+//     if (element > 13) {
+//       return index;
+//     }
+//   }),
+//   "findindex"
+// );
+
+// const isLargeNumber = (element) => element > 13;
+
+// console.log(array1.findIndex(isLargeNumber));
