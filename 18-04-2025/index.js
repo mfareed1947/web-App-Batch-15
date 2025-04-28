3e734a1ec4310e624cc4828f7f166c039792226d
Hashir346 <ahmedhashir346@gmail.com>
2025-04-28 08:09:37 -0700

    arrayMethods

---
+++ 18-04-2025/index.js
@@ -0,0 +1,37 @@
+console.log("hello")
+
+let students = ["ali", "noman", "talha", "nomans", "hamza"]
+// let numbers = [1, 4, 3, 9, 7, 10, 1000, 25, 32]
+const finded = students.findIndex((elem, index, arr) => elem == "noman")
+
+console.log(finded, "finded")
+
+
+
+// join method
+// console.log(students.join("&"))
+
+
+//includes method
+// console.log(students.includes("talha"))
+
+//slice method
+// const data = students.slice(1, 3)
+// console.log(data)
+
+// splice method
+// const splicedData = students.splice(1, 3)
+// console.log(splicedData, "splicedData")
+
+let numbers = [1, 4, 3, 9, 7, 10, 1000, 25, 32]
+// // sort method
+// const sortedData = numbers.sort((a, b) => a - b)
+// console.log(sortedData, "sortedData")
+
+// reverse method
+// students.reverse()
+// console.log(students)
+
+// concat method
+// console.log(numbers.concat(students))
+
