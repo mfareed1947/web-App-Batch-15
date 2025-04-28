ec4ee0294663ddd160712b2246063830ebd6dd1a
Hashir346 <ahmedhashir346@gmail.com>
2025-04-28 08:13:11 -0700

    filter,foreach,map,indexof

---
+++ 20-04-2024/index.js
@@ -0,0 +1,59 @@
+console.log("first")
+
+
+// console.log(array1.every((elem, index) => elem < 255))
+
+
+
+// console.log(array1.some((elem, index) => elem < 25))
+// const array1 = [1, 30, 39, 29, 10, 13];
+
+// console.log(array1.find((elem) => {
+//     return elem > 25
+// }))
+
+//filter method
+// const words = ["spray", "elite", "exuberant", "destruction", "present"];
+
+// const result = words.filter((word) => word !== "destruction");
+// console.log(result, "result")
+
+
+//forEach method
+// const array1 = [1, 30, 39, 29, 10, 13];
+
+// array1.forEach((element) => {
+//     return element * 2
+// })
+// console.log(array1)
+
+
+// map method
+
+// console.log(array1.map((elem) => (
+//     elem * 2
+// )))
+
+
+// const animals = ["hello", "Dodo", "Tiger", "Dodo", "Penguin"];
+
+// console.log(animals.lastIndexOf("Dodo"));
+
+// indexOf
+// const beasts = ["ant", "bison", "camel", "duck", "bison"];
+
+// console.log(beasts.indexOf("bison"));
+
+
+// const array1 = ["a", "b", "c"];
+
+// const iterator1 = array1.entries();
+
+// console.log(iterator1.next().value, "value");
+// console.log(iterator1.next().value, "value");
+// console.log(iterator1.next(), "next");
+// Expected output: Array [0, "a"]
+
+// console.log(iterator1.next().value);
+
+// color : "black"
